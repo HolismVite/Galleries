5de839b9e173c5107703915db33fb5b5f9983c11
Saeed Nemati <sn.1361@gmail.com>
2022-06-25 16:51:47 +0430

    committed by script

--- Admin/Exports.jsx
+++ Admin/Exports.jsx
@@ -2,8 +2,8 @@ import Galleries from './Gallery/List'
 
 const GalleryRoutes = [
     {
-        "path": "/galleries",
-        "component": Galleries
+        path: "/galleries",
+        component: Galleries
     }
 ]
 
